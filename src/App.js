2afe890588d61d9a1ddcc0faf2127c5f696893f8
cag2050 <cag2050@163.com>
2018-05-17 14:58:04 +0800

    m

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import './App.css';
 import MyLayout from './components/MyLayout'
 import {withRouter} from 'react-router'
+// import DevTools from 'mobx-react-devtools'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <MyLayout/>
+                {/*<DevTools/>*/}
             </div>
         );
     }
