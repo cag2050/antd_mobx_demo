be9c1a7caa6cd06dfb26de03e7172ec0ac1dcf37
cag2050 <cag2050@163.com>
2018-05-17 10:43:36 +0800

    m

--- src/components/InputDemo.js
+++ src/components/InputDemo.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import {Input} from 'antd';
+import {
+    observable,
+    // action
+} from 'mobx'
+import {observer} from 'mobx-react'
 
+@observer
 class InputDemo extends React.Component {
+    @observable inputValue = 'default value'
 
-  render() {
-    return (
-      <div>
-        <Input placeholder="Basic usage"></Input>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Input defaultValue={this.inputValue} placeholder="Basic usage"></Input>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        console.log(this.inputValue)
+    }
 }
 
 export default InputDemo
