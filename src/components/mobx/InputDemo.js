d003d6d1a9e92d0b8ffd6dad839582a52ac81dd2
cag2050 <cag2050@163.com>
2018-05-18 14:46:34 +0800

    m

--- src/components/mobx/InputDemo.js
+++ src/components/mobx/InputDemo.js
@@ -23,6 +23,7 @@ class InputDemo extends React.Component {
                        placeholder="Basic usage"></Input>
                 {this.inputValue}
                 <div>引用provider注入的store（利用context原理）：</div>
+                <div>input框的onChange事件发生时，执行store的action，因为会变化：</div>
                 {this.props.ChangeNameStore.name}
             </div>
         )
@@ -31,6 +32,8 @@ class InputDemo extends React.Component {
     handleChange = (e) => {
         console.log(e.target.value)
         this.form.inputValue = e.target.value
+        // 执行store的action
+        this.props.ChangeNameStore.changeName()
     }
 
     componentDidMount() {
