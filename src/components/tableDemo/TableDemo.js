d779e1986dd4b60a0b8422889f0cd3c56a0b4264
cag2050 <cag2050@163.com>
2018-05-31 15:35:03 +0800

    m

--- src/components/tableDemo/TableDemo.js
+++ src/components/tableDemo/TableDemo.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Table, Button} from 'antd'
-// import './tableDemo.css'
+import {
+    Table
+} from 'antd'
 import './tableDemo.styl'
 
 class TableDemo extends React.Component {
@@ -8,26 +9,35 @@ class TableDemo extends React.Component {
         super(props)
         this.state = {
             columnsData: [
-                { title: 'Full Name', width: 100, dataIndex: 'name', key: 'name' },
-                { title: 'Age', width: 100, dataIndex: 'age', key: 'age', },
-                { title: 'Column 1', dataIndex: 'address', key: '1', width: 150 },
-                { title: 'Column 2', dataIndex: 'address', key: '2', width: 150 },
-                { title: 'Column 3', dataIndex: 'address', key: '3', width: 150 },
-                { title: 'Column 4', dataIndex: 'address', key: '4', width: 150 },
-                { title: 'Column 5', dataIndex: 'address', key: '5', width: 150 },
-                { title: 'Column 6', dataIndex: 'address', key: '6', width: 150 },
-                { title: 'Column 7', dataIndex: 'address', key: '7', width: 150 },
-                { title: 'Column 8', dataIndex: 'address', key: '8' },
-                {
-                    title: 'Action',
-                    key: 'operation',
-                    // fixed: 'right',
-                    width: 100,
-                    render: () => <a href="javascript">action</a>,
-                },
+                {title: 'Full Name', dataIndex: 'name', key: 'name'},
+                {title: 'Full Name', dataIndex: 'name', key: 'name'},
+                {title: 'Full Name', dataIndex: 'name', key: 'name'},
+                {title: 'Full Name', dataIndex: 'name', key: 'name'},
+                {title: 'Full Name', dataIndex: 'name', key: 'name'},
+                {title: 'Full Name', dataIndex: 'name', key: 'name'},
+                {title: 'Full Name', dataIndex: 'name', key: 'name'},
+                {title: 'Full Name', dataIndex: 'name', key: 'name'},
+                {title: 'Full Name', dataIndex: 'name', key: 'name'},
+                {title: 'Full Name', dataIndex: 'name', key: 'name'},
+                {title: 'Full Name', dataIndex: 'name', key: 'name'},
+                {title: 'Column 8', dataIndex: 'address', key: '8'},
+                {title: 'Column 8', dataIndex: 'address', key: '8'},
+                {title: 'Column 8', dataIndex: 'address', key: '8'},
+                {title: 'Column 8', dataIndex: 'address', key: '8'},
+                {title: 'Column 8', dataIndex: 'address', key: '8'},
+                {title: 'Column 8', dataIndex: 'address', key: '8'},
+                {title: 'Column 8', dataIndex: 'address', key: '8'},
+                {title: 'Column 8', dataIndex: 'address', key: '8'},
+                {title: 'Column 8', dataIndex: 'address', key: '8'},
+                {title: 'Column 8', dataIndex: 'address', key: '8'},
+                {title: 'Column 8', dataIndex: 'address', key: '8'},
             ],
             dataSourceData: []
         }
+    }
+
+    componentDidMount() {
+        this.setTableData()
     }
 
     setTableData = () => {
@@ -47,16 +57,12 @@ class TableDemo extends React.Component {
 
     render() {
         return (
-            <div>
-                <Button onClick={this.setTableData}>点击后setState（设置表格数据）(做测试用，表格标题的key重复了，报错，不用管)</Button>
+            <div className='table_container'>
                 <Table
+                    // style={{width:'100%'}}
                     dataSource={this.state.dataSourceData}
                     columns={this.state.columnsData}
                     bordered
-                    // scroll={{x: true, y: 300}}
-                    // scroll={{x: 'true'}}
-                    scroll={{x: 1500, y: 300}}
-                    // scroll={{y: 300}}
                 />
             </div>
         )
