a036eda2d065947c1ee8002dff6bad7252d71e0f
cag2050 <cag2050@163.com>
2018-05-31 11:49:52 +0800

    m

--- src/components/tableDemo/TableDemo.js
+++ src/components/tableDemo/TableDemo.js
@@ -8,30 +8,23 @@ class TableDemo extends React.Component {
         super(props)
         this.state = {
             columnsData: [
-                {title: 'Full Name', dataIndex: 'name', key: 'name', width: 150},
-                {title: 'Age', dataIndex: 'age', key: 'age', width: 100},
-                {title: 'Age', dataIndex: 'age', key: '1', width: 400},
-                {title: 'Age', dataIndex: 'age', key: '2', width: 100},
-                {title: 'Age', dataIndex: 'age', key: '3', width: 200},
-                {title: 'Age', dataIndex: 'age', key: '4', width: 100},
-                {title: 'Age', dataIndex: 'age', key: '5', width: 100},
-                {title: 'Age', dataIndex: 'age', key: '6', width: 100},
-                {title: 'Age', dataIndex: 'age', key: '7', width: 300},
-                {title: 'Age', dataIndex: 'age', key: '7', width: 300},
-                {title: 'Age', dataIndex: 'age', key: '7', width: 300},
-                {title: 'Age', dataIndex: 'age', key: '7', width: 300},
-                {title: 'Age', dataIndex: 'age', key: '7', width: 300},
-                {title: 'Age', dataIndex: 'age', key: '7', width: 300},
-                {title: 'Age', dataIndex: 'age', key: '7', width: 300},
-                {title: 'Age', dataIndex: 'age', key: '7', width: 300},
-                {title: 'Age', dataIndex: 'age', key: '7', width: 300},
-                {title: 'Age', dataIndex: 'age', key: '8', width: 100},
-                {title: 'Age', dataIndex: 'age', key: '9', width: 100},
-                {title: 'Age', dataIndex: 'age', key: '10', width: 100},
-                {title: 'Age', dataIndex: 'age', key: '11', width: 100},
-                {title: 'Age', dataIndex: 'age', key: '12', width: 500},
-                {title: 'Age', dataIndex: 'age', key: '13', width: 100},
-                {title: 'Age', dataIndex: 'age', key: '14', width: 100},
+                { title: 'Full Name', width: 100, dataIndex: 'name', key: 'name' },
+                { title: 'Age', width: 100, dataIndex: 'age', key: 'age', },
+                { title: 'Column 1', dataIndex: 'address', key: '1', width: 150 },
+                { title: 'Column 2', dataIndex: 'address', key: '2', width: 150 },
+                { title: 'Column 3', dataIndex: 'address', key: '3', width: 150 },
+                { title: 'Column 4', dataIndex: 'address', key: '4', width: 150 },
+                { title: 'Column 5', dataIndex: 'address', key: '5', width: 150 },
+                { title: 'Column 6', dataIndex: 'address', key: '6', width: 150 },
+                { title: 'Column 7', dataIndex: 'address', key: '7', width: 150 },
+                { title: 'Column 8', dataIndex: 'address', key: '8' },
+                {
+                    title: 'Action',
+                    key: 'operation',
+                    // fixed: 'right',
+                    width: 100,
+                    render: () => <a href="javascript">action</a>,
+                },
             ],
             dataSourceData: []
         }
@@ -47,16 +40,7 @@ class TableDemo extends React.Component {
                 address: `London Park no. ${i}`,
             });
         }
-        const columnData = []
-        for (let i = 0; i < 100; i++) {
-            columnData.push({
-                key: i,
-                title: `Edrward ${i}`,
-                dataIndex: `London Park no. ${i}`
-            });
-        }
         this.setState({
-            // columnsData: columnData,
             dataSourceData: data
         })
     }
@@ -68,8 +52,11 @@ class TableDemo extends React.Component {
                 <Table
                     dataSource={this.state.dataSourceData}
                     columns={this.state.columnsData}
-                    scroll={{x: true, y: 300}}
-                    // scroll={{x: 100, y: 300}}
+                    bordered
+                    // scroll={{x: true, y: 300}}
+                    // scroll={{x: 'true'}}
+                    scroll={{x: 1500, y: 300}}
+                    // scroll={{y: 300}}
                 />
             </div>
         )
